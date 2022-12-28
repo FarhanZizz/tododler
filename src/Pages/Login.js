43e395b212986d413c0f6e883989f56705bfcaaa
FarhanZizz <iamfarhanaziz@gmail.com>
2022-12-28 20:36:44 +0530

    added firebase email & google sign in options

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,42 +1,80 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import img from '../Assets/Mobile login-rafiki.png'
+import { AuthContext } from '../AuthProvider/AuthProvider';
 const Login = () => {
+    const navigate = useNavigate();
+    const [error, setError] = useState('');
+    const { login, googleLogin } = useContext(AuthContext);
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/'
+
+
+    const HandleGoogleSignIn = () => {
+        googleLogin()
+            .then((result) => {
+                setError('');
+                navigate(from, { replace: true })
+            })
+            .catch((error) => {
+                const errorMessage = error.message;
+                setError(errorMessage)
+            })
+    }
+
+    const handleLoginSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        login(email, password)
+            .then((result) => {
+                form.reset();
+                setError('');
+                navigate(from, { replace: true })
+            })
+            .catch((error) => {
+                const errorMessage = error.message;
+                setError(errorMessage)
+            })
+    }
     return (
+
         <section className=" grid grid-cols-1 gap-x-10 md:px-10 md:grid-cols-2 items-center dark:bg-gray-900">
             <img className='' src={img} alt="" />
-            <div class="w-full p-4 bg-white  rounded-lg sm:p-6 md:p-8 dark:bg-gray-900 ">
-                <form class="space-y-6" action="#">
-                    <h5 class="text-2xl text-center font-medium text-gray-900 dark:text-white">Log in</h5>
-                    <div class="relative z-0">
-                        <input type="text" id="floating_standard" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-                        <label for="floating_standard" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Your Email</label>
+            <div className="w-full p-4 bg-white  rounded-lg sm:p-6 md:p-8 dark:bg-gray-900 ">
+                <form onSubmit={handleLoginSubmit} className="space-y-6" action="#">
+                    <h5 className="text-2xl text-center font-medium text-gray-900 dark:text-white">Log in</h5>
+                    <div className="relative z-0">
+                        <input type="email" name='email' className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                        <label htmlFor="floating_standard" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Your Email</label>
                     </div>
-                    <div class="relative z-0">
-                        <input type="text" id="floating_standard" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-                        <label for="floating_standard" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Your Password</label>
+                    <div className="relative z-0">
+                        <input type="password" name='password' className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                        <label htmlFor="floating_standard" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Your Password</label>
                     </div>
-                    <div class="flex items-start">
-                        <div class="flex items-start">
-                            <div class="flex items-center h-5">
-                                <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required />
+                    <div className="flex items-start">
+                        <div className="flex items-start">
+                            <div className="flex items-center h-5">
+                                <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required />
                             </div>
-                            <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+                            <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
                         </div>
-                        <a href="ww" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
+                        <a href="ww" className="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
                     </div>
-                    <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">LOG IN</button>
-                    <div class="relative flex items-center">
-                        <div class="flex-grow border-t border-gray-500"></div>
-                        <span class="flex-shrink mx-4 text-gray-500">OR</span>
-                        <div class="flex-grow border-t border-gray-500"></div>
+                    <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">LOG IN</button>
+                    <div className="relative flex items-center">
+                        <div className="flex-grow border-t border-gray-500"></div>
+                        <span className="flex-shrink mx-4 text-gray-500">OR</span>
+                        <div className="flex-grow border-t border-gray-500"></div>
                     </div>
-                    <button type="button" class=" w-full text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
-                        <svg class="mr-2 -ml-1 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>
+                    <button onClick={HandleGoogleSignIn} type="button" className=" w-full text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
+                        <svg className="mr-2 -ml-1 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>
                         Continue with Google
                     </button>
-                    <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-                        Don't Have an Account? <Link to='/signup' class="text-blue-700 hover:underline dark:text-blue-500">Sign Up Now</Link>
+                    <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+                        Don't Have an Account? <Link to='/signup' className="text-blue-700 hover:underline dark:text-blue-500">Sign Up Now</Link>
                     </div>
 
                 </form>

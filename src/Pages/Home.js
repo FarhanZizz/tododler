0e43c73072c8a47dd00f14fa1191fab05ce17f10
FarhanZizz <iamfarhanaziz@gmail.com>
2022-12-28 23:10:00 +0530

    added home page banner

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Features from '../Components/Features';
 import Hero from '../Components/Hero';
+import HomeBanner from '../Components/HomeBanner';
 
 const Home = () => {
     return (
         <div>
             <Hero></Hero>
             <Features></Features>
+            <HomeBanner></HomeBanner>
         </div>
     );
 };
